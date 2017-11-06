efce105d68b6db6af056b2ded267ff3cbd703e0f
thibenites <thiagolab97@gmail.com>
2017-11-06 15:12:06 -0200

    prima versao

--- src/main.c
+++ src/main.c
@@ -1,17 +1,80 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
+/* Programa - Primesort - THIAGO LACERDA BENITES - RA 177634 */
 #include <stdio.h>
+int ehprimo(int a);
 
-int main() {
+int main(){
+    int vetor[20];
+    int i, j, z, n;
+    j = 0;
+    z = 0;
+    n = 0;
+    
+    for(i=0; z!=1; i++){
+        scanf("%d", &vetor[i]);
+        if(vetor[i] == (-1)){
+            z = 1;
+        }
+        j++;
+    }
+    
+    j = (j-1);
+    
+    int correto[j];
+    
+    for(i=0; i<(j); i++){
+        correto[i] = vetor[i];
+    }
+    
+    int nprimo[j];
+    
+    for(i=0; i<j; i++){
+        if(ehprimo(correto[i])){
+            //do nothing
+        }
+        else{
+            nprimo[n] = correto[i];
+            n++;
+        }
+    }
+    
+    int correto_nprimo[n];
+    
+    for(i=0; i<(n); i++){
+        correto_nprimo[i] = nprimo[i];
+    }
+    
+    int aux;
+    
+    for(i=0; i<n; i++){
+        for(j=i+1; j<n; j++){
+            if(correto_nprimo[i] < correto_nprimo[j]){
+                aux = correto_nprimo[i];
+                correto_nprimo[i] = correto_nprimo[j];
+                correto_nprimo[j] = aux;
+            }
+        }
+    }
+    
+    for(i=0; i<n; i++){
+        printf("%d\n", correto_nprimo[i]);
+    }
+        
+    return 0;
+}
 
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+int ehprimo(int a){
+    int n, i;
+    n = 0;
+    i = 0;
+    for(i=1; i<=a; i++){
+        if(a%i == 0){
+            n++;
+        }
+    }
+    if(n == 2){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
